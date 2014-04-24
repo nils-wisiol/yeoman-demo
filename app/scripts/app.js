dd114b98ab20417b9a8023a47da410c8f3f8b50d
Nils Wisiol <info@nils-wisiol.de>
2014-04-24 18:30:21 -0400

    chore(): removed unnecessary dependencies, including ngRoute
    This also fixed bug with anchors on the homepage.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,19 +2,6 @@
 
 angular
   .module('wgCheetahApp', [
-    'ngCookies',
-    'ngResource',
-    'ngSanitize',
-    'ngRoute',
     'restform'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+  ]);
+
