7bfbb86d8f45670952dc650cf2cffdbb8fac10a6
Nils Wisiol <info@nils-wisiol.de>
2014-04-24 18:26:20 -0400

    chore(): removed example code

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,5 @@
 
 angular.module('wgCheetahApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope = $scope; // nothing to do
   });
